cd7bbcc7923f07d51704550560bd667a0f442d9e
leo9iota <117021032+leo9iota@users.noreply.github.com>
2026-05-12 15:58:38 +0200

    feat(main): handle required args

--- solvers/leo9iota/haskell/app/Main.hs
+++ solvers/leo9iota/haskell/app/Main.hs
@@ -1,5 +1,13 @@
--- declare main mod
-module Main where
+module Main where -- declare main mod
+
+import System.Environment (getArgs) -- import getArgs fn from system stdlib
 
 main :: IO ()
-main = putStrLn "sudoku solver written in haskell"
+main = do
+  args <- getArgs -- get cli args
+  case args of
+    [path, nStr, solverIdxStr] -> do
+      putStrLn $ "path: " ++ path
+      putStrLn $ "n: " ++ nStr
+      putStrLn $ "solver: " ++ solverIdxStr
+    _ -> putStrLn "usage: haskell-solver <PATH> <N> <SOLVER_IDX>"
